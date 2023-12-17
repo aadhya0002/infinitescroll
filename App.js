fa083f2e1fbf806fd3bbee2f26f3efbb29742d36
Parth-kumar-1802 <parthkumar1802@gmail.com>
2023-12-17 16:03:09 +0530

    infinite scroll with sanity fetching

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React,{useState,useEffect} from "react";
 import{StyleSheet,FlatList,View,Text,ActivityIndicator} from "react-native";
 import axios from "axios";
+import client from "./sanity";
 //import { red100 } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 
 
 const App=()=>{
   const [users,setUsers]=useState([]);
-  const [currentPage,setCurrentPage]=useState(1);
-  const getUsers =()=>{
-    axios.get(`https://randomuser.me/api/?page=${currentPage}&results=20`)
-    .then(res=>{
-      //setUsers(res.data.results);
-      setUsers([...users, ...res.data.results]);
-    })
-  }
+  const [currentPage,setCurrentPage]=useState(0);
+  const [data, setData] = useState([]);
+
+  
+  // let query = `*[_type == "placement23"]{...}`;
+  // client.fetch(query)
+  // .then(res=>{
+  //   setUsers(res);
+  // })
 
   const renderItem=({item})=>{
     return(
       <View style={StyleSheet.itemWrapperStyle}>
       <View style={StyleSheet.contentWrapperStyle}>
-        <Text style={styles.textNameStyle}>{`${item.name.title} ${item.name.first} ${item.name.last}`}</Text>
+        <Text style={styles.textNameStyle}>{`${item.name} ${item.name.first} ${item.name.last}`}</Text>
         <Text style={styles.textEmailStyle}>{item.email}</Text>
       </View>
       </View>
@@ -28,26 +30,35 @@ const App=()=>{
   }
 
   const renderLoader=()=>{
-    return(
-      <View style={styles.loaderStyle}>
-        <ActivityIndicator size="large" color="#aaa"/>
-      </View>
-    )
+    //{setTimeout(()=>{
+      return(
+        <View style={styles.loaderStyle}>
+            <ActivityIndicator size="large" color="#aaa"/>
+        </View>
+      )
+    //},200)}
   }
-  const loadMoreItem=()=>{
-    setCurrentPage(currentPage+1);
-  }
+
   useEffect(()=>{
-    getUsers();
+    const value  = 10 + (10*currentPage);
+    let query=`*[_type == "placement23"]{...}`;
+    client.fetch(query).then((res)=>{
+      setData(res.slice(0,value));
+    })
+    // const data = users.slice(0,value);
+    // setData(data);
+    
+
   },[currentPage])
   return(
     <FlatList
-      data={users}
+      data={data}
       renderItem={renderItem}
-      keyExtractor={item=>item.email}
+      onEndReached={()=>{
+        setCurrentPage(currentPage+1);
+      }}
+      onEndReachedThreshold={0.5}
       ListFooterComponent={renderLoader}
-      onEndReached={loadMoreItem}
-      onEndReachedThreshold={0}
     />
   )
 }
