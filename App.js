bb84f6b0dfe2ca8a76f3a7b4402d0f95c9d977f2
aadhya0002 <134799265+aadhya0002@users.noreply.github.com>
2023-12-17 14:06:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,43 +1,54 @@
 import React,{useState,useEffect} from "react";
-import{StyleSheet,FlatList,View,Text} from "react-native";
+import{StyleSheet,FlatList,View,Text,ActivityIndicator} from "react-native";
 import axios from "axios";
+//import { red100 } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 
 
 const App=()=>{
   const [users,setUsers]=useState([]);
+  const [currentPage,setCurrentPage]=useState(1);
   const getUsers =()=>{
-    axios.get(`https://randomuser.me/api/?page=3&results=10`)
+    axios.get(`https://randomuser.me/api/?page=${currentPage}&results=20`)
     .then(res=>{
-      setUsers(res.data.results);
+      //setUsers(res.data.results);
+      setUsers([...users, ...res.data.results]);
     })
   }
 
   const renderItem=({item})=>{
     return(
       <View style={StyleSheet.itemWrapperStyle}>
-      <Image style={styles.itemImageStyle} source={{uri:item.picture.large}}></Image>
       <View style={StyleSheet.contentWrapperStyle}>
         <Text style={styles.textNameStyle}>{`${item.name.title} ${item.name.first} ${item.name.last}`}</Text>
-        <Text>{item.email}</Text>
+        <Text style={styles.textEmailStyle}>{item.email}</Text>
       </View>
       </View>
     )
   }
+
+  const renderLoader=()=>{
+    return(
+      <View style={styles.loaderStyle}>
+        <ActivityIndicator size="large" color="#aaa"/>
+      </View>
+    )
+  }
+  const loadMoreItem=()=>{
+    setCurrentPage(currentPage+1);
+  }
   useEffect(()=>{
     getUsers();
-  })
+  },[currentPage])
   return(
     <FlatList
       data={users}
       renderItem={renderItem}
       keyExtractor={item=>item.email}
+      ListFooterComponent={renderLoader}
+      onEndReached={loadMoreItem}
+      onEndReachedThreshold={0}
     />
-    //<View>
-    //   <Text>
-    //     Hello world
-    //   </Text>
-    // </View>
   )
 }
 
@@ -46,8 +57,8 @@ const styles=StyleSheet.create({
     flexDirection:'row',
     paddingHorizontal:16,
     paddingVertical:16,
-    borderBottomWidth:1,
-    borderColor:'red'
+    borderBottomWidth:2,
+    backgroundColor:'#ddd'
   },
   itemImageStyle:{
     width:50,
@@ -56,6 +67,20 @@ const styles=StyleSheet.create({
   },
   contentWrapperStyle:{
     justifyContent:"space-around"
+  },
+  textNameStyle:{
+    textAlign:"center",
+    fontSize:25
+  },
+  textEmailStyle:{
+    textAlign:"center",
+    fontSize:18,
+    marginBottom:7,
+    color:'#90ee90'
+  },
+  loaderStyle:{
+    marginVertical:16,
+    alignItems:'center'
   }
 })
 
